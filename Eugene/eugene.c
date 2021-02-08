466638131558b5fc956b844aa0b9f7c65cf1269e
Eugene Chew <echew140899@gmail.com>
2021-02-08 03:04:19 +0000

    my code

--- Eugene/eugene.c
+++ Eugene/eugene.c
@@ -1,5 +1,12 @@
 int main(){
-  printf("hello");
+  int i, j, n, wa[10], tat[10], timeSlice, ct[10], a, max_process_time, temp = 0,  waiting_time = 0;
+  float awt = 0.0, att=0.0;
+  int loop;
+
+
+printf("Enter the no of processes:");
+scanf("%d", &n);
+
 
 
 }
