43af5a4b9512f87b9aef34063daeb3400a5dde8b
Harish <harish_balamurugan@yahoo.com.sg>
2021-02-13 12:20:18 +0800

    Update test.h to include cpu_time
    cpu_time was added to duplicate burst_time, so as to allow for modifications to the burst_time while cpu_time stores the original burst time.

--- test_related/test.h
+++ test_related/test.h
@@ -1,6 +1,7 @@
 typedef struct process {
     int pid;
     int burst_time;
+    int cpu_time; // Same as burst_time, but this variable will not be changed even if burst_time will be changed in the future
     int arrival_time;
     int waiting_time;
     int turnaround_time;
