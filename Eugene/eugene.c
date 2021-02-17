155da8efb80e5c4cc2918c69b0241e00d6dc85cc
Echilled <echew140899@gmail.com>
2021-02-17 22:06:05 +0800

    Update eugene.c

--- Eugene/eugene.c
+++ Eugene/eugene.c
@@ -1,83 +1,138 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct process{
+  int process_number;
+  int burst_time;
+  int arrival_time;
+  int waiting_time;
+  int turnaround_time;
+
+} Process;
 
 
 void ascending(int array[], int number){
-  int array_buffer;
-      for (int i = 0; i < number; ++i) {
-        for (int j = i + 1; j < number; ++j) {
-          if (array[i] > array[j]){
-              array_buffer = array[i];
-              array[i] = array[j];
-              array[j] = array_buffer;
-  
-            }
-        }
-      }
+	// Arranges Ready Queue according to burst time in ascending order
+  	int array_buffer;
+	for (int i = 0; i < number; ++i) {
+		for (int j = i + 1; j < number; ++j) {
+			if (array[i] > array[j]){
+				array_buffer = array[i];
+				array[i] = array[j];
+				array[j] = array_buffer;
+			}
+		}
+	}
 }
 
+int main(void) {
+	// Main program
+  	int i, j, n, et_buffer, loop,tat[10], TQ, ct[10], max_process_time, response_times[10], max_arrival_time, size_t, lmao_buffer;
+  	float awt = 0.0, att=0.0, art=0.0;
 
-int main(void) { // Main program
-  int i, j, n, et_buffer, loop,tat[10], TQ, ct[10], max_process_time, burst_times[10], response_times[10];
-  float awt = 0.0, att=0.0, art=0.0;
 
 
 printf("Enter the no of processes:");
 scanf("%d", &n);
 
-int arr_times[n], process_q[n], exe_times[n], wait_times[n], ready_q[n];
-int process_buffer, ex_buffer;
+struct process process[n]; //Creating the number of processes
+
+for(i = 0; i < n; i++ ) //Getting process inputs
+    {
+        printf("Enter burst time for process %d:", i+1);
+        scanf("%d", &process[i].burst_time);
+
+
+        printf("Enter Arrival time for process %d:", i+1);
+        scanf("%d", &process[i].arrival_time);
+
+        process[i].process_number = i+1;
+    }
+
+
+
+	//Getting the time quantum
+	printf("\nEnter the size of time quantum:");
+	scanf("%d", &TQ);
+
+
+//Finding biggest process time
+  max_process_time = 0;
+	for(i = 0; i < n; i++){
+       
+    	if(max_process_time < process[i].burst_time){
+			  max_process_time = process[i].burst_time;
+		}
+  	}
+
+//Find latest arrival
+  max_arrival_time = 0;
+	for(i = 0; i < n; i++){
+    	if(max_arrival_time < process[i].arrival_time){
+			  max_arrival_time = process[i].arrival_time;
+		}
+  	}
+
 
 
 for(i = 0; i < n; i++){
-  printf("\nEnter Execution Time for process %d: ",i + 1);
-  scanf("%d", &exe_times[i]); //Storing burst times into the burst time array
+  printf("Process %d \t burst time: %d \t arrival time: %d\n", i+1, process[i].burst_time, process[i].arrival_time);
 }
 
-for(i = 0; i < n; i++){
-  printf("\nEnter Arrival Time for process %d: ",i + 1);
-  scanf("%d", &arr_times[i]); //Storing arrival times into the burst time array
-}
 
-//Getting the time quantum
-printf("\nEnter the size of time quantum:");
-scanf("%d",&TQ);
-max_process_time=exe_times[0];
+printf("The max process time: %d\n", max_process_time);
+printf("The latest arrival time: %d", max_arrival_time);
 
 
-for(i=1;i<n;i++) //Finding the biggest process time among the processes
-  {
-    if(max_process_time<exe_times[i])
-      max_process_time=exe_times[i];
-  }
+int ready_q[n];
+ready_q[0] = 0; //Control variable to check if array is empty
+
+
+
+	// int arr_times[n], process_q[n], burst_times[n], wait_times[n], ready_q[n];
+	// int process_buffer, ex_buffer;
+
+
+	// for(i = 0; i < n; i++){
+  // 		printf("\nEnter Execution Time for process %d: ",i + 1);
+  // 		scanf("%d", &burst_times[i]); //Storing burst times into the burst time array
+	// }
+
+	// for(i = 0; i < n; i++){
+  // 		printf("\nEnter Arrival Time for process %d: ",i + 1);
+  // 		scanf("%d", &arr_times[i]); //Storing arrival times into the burst time array
+	// }
+
+
   
 
-//Sorting processes based on arrival time (ascending order)
+	//Sorting processes based on arrival time (ascending order)
 
-//This is only the initial arrangement
-for(i = 0; i< n; i++){ //populating the queue with processes
-    process_q[i] = i+1;
-    //printf("Process: %d", process_q[i]);
-}
+	//This is only the initial arrangement
+	// for(i = 0; i< n; i++){ //populating the queue with processes
+  //   	process_q[i] = i+1;
+  //   	//printf("Process: %d", process_q[i]);
+	// }
 
-for (i = 0; i < n; ++i) {
-  for (j = i + 1; j < n; ++j) {
-    if (arr_times[i] > arr_times[j]){
-      et_buffer =  arr_times[i];
-      arr_times[i] = arr_times[j];
-      arr_times[j] = et_buffer;
+	// // Arrange burst time based on arr time
+	// for (i = 0; i < n; ++i) {
+  // 		for (j = i + 1; j < n; ++j) {
+	// 		if (arr_times[i] > arr_times[j]){
+	// 		et_buffer =  arr_times[i];
+	// 		arr_times[i] = arr_times[j];
+	// 		arr_times[j] = et_buffer;
 
-      process_buffer = process_q[i]; //Inital arrangement based on arrival time
-      process_q[i] = process_q[j];
-      process_q[j] = process_buffer;
+	// 		process_buffer = process_q[i]; //Inital arrangement based on arrival time
+	// 		process_q[i] = process_q[j];
+	// 		process_q[j] = process_buffer;
 
-      ex_buffer = exe_times[i];
-      exe_times[i] = exe_times[j];
-      exe_times[j] = ex_buffer;
-      
-      
-    }
-  }
-}
+	// 		ex_buffer = burst_times[i];
+	// 		burst_times[i] = burst_times[j];
+	// 		burst_times[j] = ex_buffer;
+	// 		}
+  // 		}
+	// }
 // // NEED to arrange based on shortest remaining time left after first process is allocated
 
 // for(i=1;i<n;i++) //Finding the biggest process time among the processes
@@ -86,55 +141,142 @@ for (i = 0; i < n; ++i) {
 //       max_process_time=exe_times[i];
 //   }
 
+#pragma region Eugene code
+	// int ts = 0;	//Initialize timestamp to 0
+	// int max_arr_time = 0;
+	// int ready_queue[n];
+	// int last_addition_timestamp = -1;
+
+	// Find last arr time
+	// for (i = 0; i < n; i++){
+	// 	if(arr_times[i] > max_arr_time){
+	// 		max_arr_time = arr_times[i];
+	// 	}
+	// }
+
+	// printf("\n **DEBUG**\n\nLast Arrival time: %d\n", max_arr_time);
+
+	// while (ts <= max_arr_time + max_process_time){
+	// 	// Check for new processes
+	// 	for (i = 0; i < n; i++){
+	// 		if(last_addition_timestamp < arr_times[i] && arr_times[i] <= ts){
+	// 			ready_queue[i] = i;
+	// 			printf("In Ready Queue: P%d at t=%d\n", ready_queue[i]+1, ts);
+	// 		} 
+	// 	}
+	// 	last_addition_timestamp = ts;
+	// 	// printf("CP1\n");
+
+	// 	bool exec = false;
+	// 	for(i=0; i < n; i++){
+	// 		if(burst_times[ready_queue[i]] > 0 && ready_queue[i] < n){
+	// 			exec = true;
+
+	// 			printf("n = %d\n", i);
+	// 			printf("process: %d\n", ready_queue[i]+1);
+	// 			printf("Burst time: %d\n", burst_times[ready_queue[i]]);
+
+	// 			// Execute the process remember to populate the burst times
+	// 			if(burst_times[ready_queue[i]] > TQ){
+	// 				ts = ts + TQ;
+	// 				burst_times[ready_queue[i]] = burst_times[ready_queue[i]] - TQ;
+	// 				printf("Executed P%d, remaining burst time: %dms\n", ready_queue[i]+1, burst_times[ready_queue[i]]);
+	// 			}
+	// 			else{
+	// 				ts = ts + burst_times[ready_queue[i]];
+	// 				burst_times[ready_queue[i]] = 0;
+	// 				printf("Executed P%d, remaining burst time: %dms\n", ready_queue[i]+1, burst_times[ready_queue[i]]);
+	// 			} 
+	// 		}
+	// 	}
 
 
-//Need a timer to keep track based on the arrival times
-int timer = 0;
+	// 	// If notthing was executed, increment timestamp by 1
+	// 	if(!exec){
+	// 		printf("This shouldnt show");
+	// 		ts++;
+	// 	}
 
-  
-while (timer < 100){
-        for(i=0; i <n; i++){
-          if (timer == arr_times[i]){
-            
-            //Add to ready queue
-            ready_q[i] = process_q[i];
-            printf("\nThis process %d is inserted at %d seconds\n", process_q[i], timer);
+	// 	printf("CP2\n");
+	// }
 
-            // Allocate CPU to the first process
-            printf("Execution time: %d", exe_times[i]);
-            exe_times[i] = exe_times[i] - TQ;
-            printf("\nWhats left %d",exe_times[i]);
-            //If execution time != 0; add it to the back of the queue
+// #pragma endregions
 
+// //Need a timer to keep track based on the arrival times
 
-            //Arranging execution times
-            for (int u = 0; u < n; ++u) {
-              for (int o = u + 1; o < n; ++o) {
-                ex_buffer = exe_times[u];
-                exe_times[u] = exe_times[o];
-                exe_times[o] = ex_buffer;
-              }
-            }
-            printf("\nNext in Ready Queue: Process %d", ready_q[i]);
-            printf("\nCurrent time %d", timer);
-            
+	int timer = 0;
+	while (timer <= max_arrival_time + max_process_time){
+		for(i=0; i < max_process_time; i++){
+			if (timer == process[i].arrival_time){
+				
+				//Add to ready queue
+				ready_q[i] = process[i].process_number;
+				printf("\nThis process %d is inserted at %d seconds\n", process[i].process_number, timer);
 
-         
-            
-          }
+				// Aft adding still compare the exe times then allocate CPU
 
+        //   if (process[i].burst_time <= TQ){
+        //     process[i].burst_time = 0;
+        //     printf("time left for process %d: %d", process[i].process_number, process[i].burst_time);
+        // }  
+        //   else{
+        //   process[i].burst_time = process[i].burst_time - TQ;
+        //   printf("time left for process %d: %d", process[i].process_number, process[i].burst_time);
+        // }
+
+      }
+      else{ //If not new process added to queue
+          if(ready_q[0] != 0){
+           for(i = 0; i<n; ++i){
+             for(j = i + 1 ; j<n; ++j)
+             if(process[ready_q[i]].burst_time > process[ready_q[j]].burst_time){
+                  lmao_buffer = process[ready_q[i]].burst_time;
+				          process[ready_q[i]].burst_time = process[ready_q[j]].burst_time;
+				          process[ready_q[j]].burst_time = lmao_buffer;
+             }
+           }
+            if (process[ready_q[i]].burst_time <= TQ){
+            process[ready_q[i]].burst_time = 0;
+        
+        }  
+            else{
+            process[ready_q[i]].burst_time = process[ready_q[i]].burst_time - TQ;
+           
         }
+          }
+        }
+
+
+      }
+
+
+
+
+        //Compare execution times Again
+
+
+				// printf("\nWhats left %d",burst_times[i]);
+				// //If execution time != 0; add it to the back of the queue
+
+
+				// //Arranging execution times
+				// // for (int u = 0; u < n; ++u) {
+				// // for (int o = u + 1; o < n; ++o) {
+				// // 	ex_buffer = burst_times[u];
+				// // 	burst_times[u] = burst_times[o];
+				// // 	burst_times[o] = ex_buffer;
+				// // }
+				// // }
+				// printf("\nNext in Ready Queue: Process %d", ready_q[i]);
+		
+	timer++;
+  printf("\nCurrent time %d", timer);
+	}
+  }
   
-      
-  timer++;
- 
+  
 
-}
-
-
-
-
-      //timer++;
+    //timer++;
   
       
     //     ready_q[timer] = process_q[timer];    //Add process to the queue if time is reached
@@ -188,6 +330,7 @@ while (timer < 100){
 
     
 
-    }
+    
   
 
+  
